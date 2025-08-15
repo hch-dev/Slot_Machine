9412659ba25cfada1074891aa6e487f95ff540ad
Harsh Choudhari <163522076+harsh876876@users.noreply.github.com>
2025-08-15 07:36:50 +0530

    Remade options menu in Print class

--- Main.java
+++ Main.java
@@ -4,6 +4,6 @@ class Main {
 
     public static void main(String args[]) {
         Print pr = new Print();
-        pr.playgame();
+        pr.details();
     }
 }
--- Print.java
+++ Print.java
@@ -19,27 +19,40 @@ public class Print {
                 """;
     }
 
-    public void playgame() {
+    public void details() {
         Scanner in = new Scanner(System.in);
+
         while (true) {
             try {
                 System.out.print("Enter bet amount: ");
                 String input = in.nextLine(); // Accepts the bet amount
                 if (!Character.isDigit(input.charAt(input.length() - 1)))
                     throw new NumberFormatException("Error");
-                    //Checks if the last character is a digit
+                // Checks if the last character is a digit
                 betamt = Double.parseDouble(input);
                 break;
             } catch (NumberFormatException e) {
+                System.out.print("\033[H\033[2J"); //Clears terminal
+                System.out.flush();
                 System.out.println("Enter a valid number");
             }
         }
 
-        System.out.println("Enter 1 to spin the reels");
-        System.out.println("Enter 2 to exit");
-        int ch = in.nextInt();
-        if (ch == 2)
-            System.exit(0);
+        do {
+            System.out.println("Enter 1 to spin the reels");
+            System.out.println("Enter 2 to exit");
+            String input = in.nextLine();
+            if (input.equals("1"))
+                break;
+            else if (input.equals("2"))
+                System.exit(0);
+            else {
+                System.out.print("\033[H\033[2J"); //Clears terminal
+                System.out.flush();
+                System.out.println("Enter a valid option");
+            }
+        } while (true);
+        // Accepts only valid answers
         in.close();
     }
 }
