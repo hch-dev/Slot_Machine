50d551374f91ac2ddf5a4ee12094164553e1b301
Harsh Choudhari <163522076+harsh876876@users.noreply.github.com>
2025-08-15 07:48:10 +0530

    Added weights for the reels

--- Reel1.java
+++ Reel1.java
@@ -1,6 +1,6 @@
 package Slot_Machine;
 
-import java.util.HashMap;
+import java.util.*;
 
 public class Reel1 {
 
@@ -9,5 +9,11 @@ public class Reel1 {
     Reel1() {
         reel1 = new HashMap<>();
         reel1.put("7️⃣", 14);
+        reel1.put("💎", 51);
+        reel1.put("🪙", 62);
+        reel1.put("♠️", 102);
+        reel1.put("🔔", 218);
+        reel1.put("🧨", 304);
+        reel1.put("🍒", 273);
     }
 }
--- Reel3.java
+++ Reel3.java
@@ -1,5 +1,19 @@
 package Slot_Machine;
 
+import java.util.*;
+
 public class Reel3 {
-    
+
+    public HashMap<String, Integer> reel3;
+
+    Reel3() {
+        reel3 = new HashMap<>();
+        reel3.put("7️⃣", 10);
+        reel3.put("💎", 49);
+        reel3.put("🪙", 64);
+        reel3.put("♠️", 94);
+        reel3.put("🔔", 212);
+        reel3.put("🧨", 308);
+        reel3.put("🍒", 287);
+    }
 }
