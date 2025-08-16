99423592a02dde9cbffad579d28b8ec7f6eb4680
Harsh Choudhari <163522076+harsh876876@users.noreply.github.com>
2025-08-16 19:33:56 +0530

    Added payout structure

--- Payout.java
+++ Payout.java
@@ -1,5 +1,21 @@
 package Slot_Machine;
 
+import java.util.*;
+
 public class Payout {
-    
+
+    public static HashMap<String, Integer> payout3;
+    public static double payout2;
+
+    Payout() {
+        payout2 = 0.0;
+
+        payout3.put("7️⃣", 500);
+        payout3.put("💎", 200);
+        payout3.put("🪙", 100);
+        payout3.put("♠️", 68);
+        payout3.put("🔔", 20);
+        payout3.put("🧨", 10);
+        payout3.put("🍒", 5);
+    }
 }
