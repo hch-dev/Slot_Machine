dd6c7ff565a4c735b19dd34406693b5929d77ced
Harsh Choudhari <163522076+hch-dev@users.noreply.github.com>
2025-08-18 20:54:55 +0530

    Checks what type of payout to give

--- Main.java
+++ Main.java
@@ -9,11 +9,14 @@ class Main {
     public static void main(String args[]) throws InterruptedException {
         Reel rl = new Reel();
         Print pr = new Print();
-        
+        Payout py = new Payout();
+
         String[] win = rl.spinreels();
         System.out.println(win[0]);
         System.out.println(win[1]);
         System.out.println(win[2]);
+
+        py.result(win);
 
     }
 }
--- Payout.java
+++ Payout.java
@@ -4,12 +4,13 @@ import java.util.*;
 
 public class Payout {
 
-    public static HashMap<String, Integer> payout3;
-    public static double payout2;
+    public static HashMap<String, Integer> payout3; // Stores payout structure
+    public static double payout2; // Stores payout for 2 of a kind
+    String type; // Stores the type of final result
 
     Payout() {
         payout2 = 0.0;
-        
+
         payout3.put("7️⃣", 500);
         payout3.put("💎", 200);
         payout3.put("🪙", 100);
@@ -17,5 +18,14 @@ public class Payout {
         payout3.put("🔔", 20);
         payout3.put("🧨", 10);
         payout3.put("🍒", 5);
+    }
+
+    void result(String[] win) {
+        if (win[0].equals(win[1]) && win[1].equals(win[2]))
+            type = "Three";
+        else if (!win[0].equals(win[1]) && !win[1].equals(win[2]) && !win[0].equals(win[2]))
+            type = "None";
+        else
+            type = "Two";
     }
 }
